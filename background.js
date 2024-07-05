43d5cc89d5d537ca9e8fe7859f6f269d1af82e69
sayeedzh <sayeed.705@gmail.com>
2024-07-06 00:00:13 +0530

    errors eleaned up

--- background.js
+++ background.js
@@ -1,26 +1,18 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (
-    // tab.url &&
-    // tab.url.includes("sunnxt.com") &&
-    changeInfo.status === "complete"
-  ) {
-    const queryParameters = tab.url.split("?")[1];
-    const urlParameters = new URLSearchParams(queryParameters);
-
-    chrome.scripting.executeScript(
-      {
-        target: { tabId: tabId },
-        func: () => document.documentElement.outerHTML,
-      },
-      (results) => {
-        const domContent = results?.[0].result;
-        chrome.tabs.sendMessage(tabId, {
-          type: "NEW",
-          videoId: urlParameters.get("v"),
-          domContent: domContent,
-          url: tab.url,
-        });
-      }
-    );
-  }
-});
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//   if (changeInfo.status === "complete") {
+//     chrome.scripting.executeScript(
+//       {
+//         target: { tabId: tabId },
+//         func: () => document.documentElement.outerHTML,
+//       },
+//       (results) => {
+//         const domContent = results?.[0].result;
+//         chrome.tabs.sendMessage(tabId, {
+//           type: "NEW",
+//           domContent: domContent,
+//           url: tab.url,
+//         });
+//       }
+//     );
+//   }
+// });
